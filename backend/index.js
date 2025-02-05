23de6f30cc4cdc926da6649c5ae16e2f1a6c929e
BS-Joy <badhansamadder5@gmail.com>
2025-02-05 23:24:52 +0600

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -11,4 +12,5 @@ app.use("/api/auth", authRouter);
 
 app.listen(5000, () => {
   console.log("server is running on port: ", port);
+  connectDB();
 });
