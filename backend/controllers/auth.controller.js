8e07bd76b1acf2e9e1ea461c88342dca59319d9e
BS-Joy <badhansamadder5@gmail.com>
2025-02-11 23:22:27 +0600

    created signup api but not working

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,43 @@
-export const signupController = (req, res) => {
-  res.send("Sign up route");
+import userModel from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/index.js";
+
+export const signupController = async (req, res) => {
+  const { email, fullName, password } = req.body;
+  console.log(req.body);
+  try {
+    // hash password
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters or more." });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "Email already exists." });
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await userModel.create({
+      email,
+      fullName,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser?._id, res);
+      console.log(newUser);
+      res.status(201).json(newUser);
+    } else {
+      res.status(400).json({ message: "Failed to create user." });
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 export const loginController = (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ import express from "express";
 import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
+
 const port = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/auth", authRouter);
 
